782954f16495f4ee5e89417683b1936b8346a4de
Sebastien Binet <seb@sebastiens-imac.local.lan>
2011-11-02 08:24:48 -0400

    Initial Commit

---
+++ SebasTest001/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SebasTest001
+//
+//  Created by Sebastien Binet on 11-11-02.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SebasTest001/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SebasTest001
+//
+//  Created by Sebastien Binet on 11-11-02.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
